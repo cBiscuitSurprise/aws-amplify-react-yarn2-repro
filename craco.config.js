7ba2c3017c4e0ad9005d3facac3ec77b7b9683ca
Casey Boyer <caseyb1101@gmail.com>
2021-12-30 23:53:16 -0600

    reproduced :cry:

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          // buffer: require.resolve('buffer'),
+          // url: require.resolve('url'),
+          buffer: false,
+          url: false,
+        }
+      }
+    }
+  }
+};
